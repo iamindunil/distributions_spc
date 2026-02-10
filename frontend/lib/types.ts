d626b8713ca7afc7b81a65cbfeeff1bd0c8a2d98
Indunil Kavinda <indunilkavinda1121@gmail.com>
2026-02-10 11:56:18 +0530

    initial commit

---
+++ frontend/lib/types.ts
@@ -0,0 +1,15 @@
+export interface Employee {
+id: number
+name: string
+email: string
+role: string
+}
+
+
+export interface Vehicle {
+id: number
+driver: string
+route: string
+status: string
+capacity: number
+}
