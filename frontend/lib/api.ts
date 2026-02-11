ffbea5da79c92c993b7e1d80cea07a9b05ccd62e
Indunil Kavinda <indunilkavinda1121@gmail.com>
2026-02-11 14:38:51 +0530

    initial commit

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -9,7 +9,7 @@ deleteEmployee: (id:number)=> fetch(`${BASE}/employees/${id}`,{method:"DELETE"})
 
 
 getVehicles: () => fetch(`${BASE}/vehicles`).then(r => r.json()),
-addVehicle: (data:string)=> fetch(`${BASE}/vehicles`,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(data)}),
-updateVehicle: (id:number,data:string)=> fetch(`${BASE}/vehicles/${id}`,{method:"PUT",headers:{"Content-Type":"application/json"},body:JSON.stringify(data)}),
+addVehicle: (data:any)=> fetch(`${BASE}/vehicles`,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(data)}),
+updateVehicle: (id:number,data:any)=> fetch(`${BASE}/vehicles/${id}`,{method:"PUT",headers:{"Content-Type":"application/json"},body:JSON.stringify(data)}),
 deleteVehicle: (id:number)=> fetch(`${BASE}/vehicles/${id}`,{method:"DELETE"}),
 }
